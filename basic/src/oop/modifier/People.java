6d04254d13ded56c1b05df1f868bbbdf2a674dec
Michael728 <649168982@qq.com>
2021-03-28 22:22:36 +0800

    update

---
+++ basic/src/oop/modifier/People.java
@@ -0,0 +1,15 @@
+package oop.modifier;
+
+/**
+ * description:
+ *
+ * @author Michael
+ * @date 2020/11/23
+ * @time 11:56 下午
+ */
+public class People {
+    public String name;
+    protected int age;
+    String sex;
+    private String hobby;
+}
---
+++ basic/src/oop/modifier/Test.java
@@ -0,0 +1,18 @@
+package oop.modifier;
+
+/**
+ * description:
+ *
+ * @author Michael
+ * @date 2020/11/23
+ * @time 11:56 下午
+ */
+public class Test {
+    public static void main(String[] args) {
+        People p = new People();
+        System.out.println(p.name);
+        System.out.println(p.age);
+        System.out.println(p.sex);
+        System.out.println(p.);
+    }
+}
